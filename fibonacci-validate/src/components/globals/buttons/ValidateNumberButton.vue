913a8aa446ca33baa993ff9e1e0f311a5ace3494
Gabi Farias <gabijk1234row@gmail.com>
2024-02-08 21:17:44 -0400

    [UPDATE] function of verification

--- fibonacci-validate/src/components/globals/buttons/ValidateNumberButton.vue
+++ fibonacci-validate/src/components/globals/buttons/ValidateNumberButton.vue
@@ -1,16 +1,9 @@
 <template>
-    <button
-      click="validate"
-      class="flex items-center rounded-md bg-secundary text-white drop-shadow-orange-0.6 hover:drop-shadow-orange-1">
-      <div class="m-2">
-        <ValidateIcon />
-      </div>
-      <p class="m-2">
-        Verificar
-      </p>
-    </button>
-  </template>
-  
-  <script setup>
-    import ValidateIcon from '@/components/icons/ValidateIcon.vue'
-  </script>
+  <button class="text-h4 text-white bg-black">
+    <p>Verificar</p>
+  </button>
+</template>
+
+<script setup>
+
+</script>
--- fibonacci-validate/src/components/globals/input/NumberInput.vue
+++ fibonacci-validate/src/components/globals/input/NumberInput.vue
@@ -1,13 +1,47 @@
-
-import ValidateNumberButton from '../buttons/ValidateNumberButton.vue';
 <template>
     <div class="flex flex-row p-2">
-        <input  id="number" type="number"  placeholder="Digite um numero"
-            class="size-12 w-56 cursor-text p-4 m-2 appearance-none border-2 border-solid border-primary">
-        <ValidateNumberButton />
+      <input
+        id="number"
+        type="number"
+        placeholder="Digite um numero"
+        class="size-12 w-56 cursor-text p-4 m-2 appearance-none border-2 border-solid border-primary"
+      />
+      <ValidateNumberButton @click="validateNumber" />
     </div>
-</template>
+    <div v-if="resultMessage" class="result-container">
+      <h3>{{ resultMessage }}</h3>
+    </div>
+  </template>
+  
+  <script>
+  import ValidateNumberButton from '../buttons/ValidateNumberButton.vue'
+  
+  export default {
+    components: {
+      ValidateNumberButton,
+    },
+    data() {
+      return {
+        resultMessage: '',
+      }
+    },
+    methods: {
+      validateNumber() {
+        let number = parseInt(document.getElementById('number').value)
+  
+        this.resultMessage = isFibonacci(number)
+          ? 'É um número de Fibonacci!'
+          : 'Não é um número de Fibonacci!'
+      },
+    },
+  }
+  function isFibonacci(n) {
+    return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4)
+}
 
-<script setup>
-import ValidateNumberButton from '../buttons/ValidateNumberButton.vue'
+function isPerfectSquare(x) {
+    let s = Math.sqrt(x)
+    return s * s === x
+}
 </script>
+  
